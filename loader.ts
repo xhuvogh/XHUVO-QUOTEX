1374036d21240e7e958da41f9bb35de7cf66c858
XHUVO <xhuvoofficial@gmail.com>
2026-08-13 07:40:02 +0000

    Delete all files from repository

--- loader.ts
+++
@@ -1,9 +0,0 @@
-// Custom event helper for triggering the global page transition overlay (Default duration: 1000ms)
-export const triggerLogoLoader = (message = 'Switching Webpage...', duration = 450, callback?: () => void) => {
-  window.dispatchEvent(
-    new CustomEvent('xhuvo-trigger-loader', {
-      detail: { message, duration, callback }
-    })
-  );
-};
-
